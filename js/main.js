802c55b638b2aa9afe1c21050e8d1698ad72dc15
Old-Yaryi <kinomir-v@yandex.ru>
2022-12-09 21:05:22 +0300

    'example_desctop'

--- js/main.js
+++ js/main.js
@@ -1,19 +1,36 @@
 $(document).ready(function(){
 
-  // $('.example__slider').slick({
-  //   arrows: false,
-  //   dots: true,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   autoplay: true,
-  //   autoplaySpeed: 2000
-  // });
+  $('.example__slider').slick({
+    arrows: true,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendArrows:'.example__slider-controls'
+  });
 
 
   $('#aics').anyImageComparisonSlider({
     initialPosition: 0.5,
     autoAnimation: false,
     cursor: 'ew-resize',
-    dividingLine: 'solid 3px rgba(255, 255, 255, 1)'
+    dividingLine: 'solid 2px rgba(255, 255, 255, 1)'
+  });
+  $('#aics-2').anyImageComparisonSlider({
+    initialPosition: 0.5,
+    autoAnimation: false,
+    cursor: 'ew-resize',
+    dividingLine: 'solid 2px rgba(255, 255, 255, 1)'
+  });
+  $('#aics-3').anyImageComparisonSlider({
+    initialPosition: 0.5,
+    autoAnimation: false,
+    cursor: 'ew-resize',
+    dividingLine: 'solid 2px rgba(255, 255, 255, 1)'
+  });
+  $('#aics-4').anyImageComparisonSlider({
+    initialPosition: 0.5,
+    autoAnimation: false,
+    cursor: 'ew-resize',
+    dividingLine: 'solid 2px rgba(255, 255, 255, 1)'
   });
 });
